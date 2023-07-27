4702231bbdddcca45ea707bc61f5ec414fa146ca
LanJing Li <l478li@uwaterloo.ca>
2023-07-27 15:44:41 -0400

    Completed Hard Coded Companion Page

--- app/src/main/java/com/example/ece489acompanionapp/ui/companion/CompanionFragment.kt
+++ app/src/main/java/com/example/ece489acompanionapp/ui/companion/CompanionFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
+import com.example.ece489acompanionapp.R
 import com.example.ece489acompanionapp.databinding.FragmentCompanionBinding
+import com.example.ece489acompanionapp.ui.tracker.TrackerViewModel
 
 class CompanionFragment : Fragment(0) {
 
     private var _binding: FragmentCompanionBinding? = null
-
+    private val sharedViewModel: CompanionViewModel by activityViewModels()
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -22,16 +26,12 @@ class CompanionFragment : Fragment(0) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
+        val companionViewModel =
             ViewModelProvider(this).get(CompanionViewModel::class.java)
 
         _binding = FragmentCompanionBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textCompanion
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 

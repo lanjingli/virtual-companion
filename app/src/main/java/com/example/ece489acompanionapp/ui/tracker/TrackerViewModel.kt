8efc644cdab04853abe4d9d294641396ee44f457
LanJing Li <l478li@uwaterloo.ca>
2023-07-26 22:34:46 -0400

    Completed Water Tracker Page

--- app/src/main/java/com/example/ece489acompanionapp/ui/tracker/TrackerViewModel.kt
+++ app/src/main/java/com/example/ece489acompanionapp/ui/tracker/TrackerViewModel.kt
@@ -1,13 +1,46 @@
 package com.example.ece489acompanionapp.ui.tracker
 
+import android.widget.ImageButton
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.ece489acompanionapp.R
 
 class TrackerViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is tracker Fragment"
     }
+
+    //private val _water =  MutableLiveData<MutableList<Boolean>>()
+    private val _water: MutableLiveData<MutableList<Boolean>> by lazy {
+        MutableLiveData<MutableList<Boolean>>().apply {
+            value = MutableList(10) { true }
+        }
+    }
+
+    private val _curWaterIntake = MutableLiveData<Int>(0)
+
     val text: LiveData<String> = _text
+
+    fun setWaterIsFull(ind: Int, isFull: Boolean) {
+        //val cur = _water.value?.get(ind)
+        _water.value?.set(ind, isFull)
+    }
+
+    fun getWaterState(ind:Int): Boolean? {
+        return _water.value?.get(ind)
+    }
+
+    fun saveWaterIntake() {
+        var count = 0
+        _water.value?.forEach { intake->
+            if (intake == true) {
+                count++
+            }
+        }
+        _curWaterIntake.value = count
+    }
+
 }
+
--- app/src/main/java/com/example/ece489acompanionapp/ui/tracker/WaterTrackerFragment.kt
+++ app/src/main/java/com/example/ece489acompanionapp/ui/tracker/WaterTrackerFragment.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.ece489acompanionapp.R
 import com.example.ece489acompanionapp.databinding.FragmentTrackerWaterBinding
-import com.example.ece489acompanionapp.ui.calendar.WaterTrackerViewModel
 
 class WaterTrackerFragment : Fragment() {
-
+    private val sharedViewModel: TrackerViewModel by activityViewModels()
     private var _binding: FragmentTrackerWaterBinding? = null
 
     // This property is only valid between onCreateView and
@@ -23,17 +24,153 @@ class WaterTrackerFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val waterTrackerViewModel =
-            ViewModelProvider(this).get(WaterTrackerViewModel::class.java)
+        val trackerViewModel =
+            ViewModelProvider(this).get(TrackerViewModel::class.java)
 
         _binding = FragmentTrackerWaterBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.trackerWater
-        waterTrackerViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            viewModel = sharedViewModel
+        }
+
+        binding?.apply {
+            water1.setOnClickListener {
+                val isFull = viewModel?.getWaterState(0)
+                var tmp = isFull
+                tmp = if (isFull == true) {
+                    water1.setImageResource(R.drawable.ic_water_full_48dp)
+                    false
+                } else {
+                    water1.setImageResource(R.drawable.ic_water_empty_48dp)
+                    true
+                }
+                viewModel?.setWaterIsFull(0, tmp)
+            }
+
+            water2.setOnClickListener {
+                val isFull = viewModel?.getWaterState(1)
+                var tmp = if (isFull == true) {
+                    water2.setImageResource(R.drawable.ic_water_full_48dp)
+                    false
+                } else {
+                    water2.setImageResource(R.drawable.ic_water_empty_48dp)
+                    true
+                }
+                viewModel?.setWaterIsFull(1, tmp)
+            }
+
+            water3.setOnClickListener {
+                val isFull = viewModel?.getWaterState(2)
+                var tmp = if (isFull == true) {
+                    water3.setImageResource(R.drawable.ic_water_full_48dp)
+                    false
+                } else {
+                    water3.setImageResource(R.drawable.ic_water_empty_48dp)
+                    true
+                }
+                viewModel?.setWaterIsFull(2, tmp)
+            }
+
+            water4.setOnClickListener {
+                val isFull = viewModel?.getWaterState(3)
+                var tmp = if (isFull == true) {
+                    water4.setImageResource(R.drawable.ic_water_full_48dp)
+                    false
+                } else {
+                    water4.setImageResource(R.drawable.ic_water_empty_48dp)
+                    true
+                }
+                viewModel?.setWaterIsFull(3, tmp)
+            }
+
+            water5.setOnClickListener {
+                val isFull = viewModel?.getWaterState(4)
+                var tmp = if (isFull == true) {
+                    water5.setImageResource(R.drawable.ic_water_full_48dp)
+                    false
+                } else {
+                    water5.setImageResource(R.drawable.ic_water_empty_48dp)
+                    true
+                }
+                viewModel?.setWaterIsFull(4, tmp)
+            }
+
+            water6.setOnClickListener {
+                val isFull = viewModel?.getWaterState(5)
+                var tmp = if (isFull == true) {
+                    water6.setImageResource(R.drawable.ic_water_full_48dp)
+                    false
+                } else {
+                    water6.setImageResource(R.drawable.ic_water_empty_48dp)
+                    true
+                }
+                viewModel?.setWaterIsFull(5, tmp)
+            }
+
+            water7.setOnClickListener {
+                val isFull = viewModel?.getWaterState(6)
+                var tmp = if (isFull == true) {
+                    water7.setImageResource(R.drawable.ic_water_full_48dp)
+                    false
+                } else {
+                    water7.setImageResource(R.drawable.ic_water_empty_48dp)
+                    true
+                }
+                viewModel?.setWaterIsFull(6, tmp)
+            }
+
+            water8.setOnClickListener {
+                val isFull = viewModel?.getWaterState(7)
+                var tmp = if (isFull == true) {
+                    water8.setImageResource(R.drawable.ic_water_full_48dp)
+                    false
+                } else {
+                    water8.setImageResource(R.drawable.ic_water_empty_48dp)
+                    true
+                }
+                viewModel?.setWaterIsFull(7, tmp)
+            }
+
+            water9.setOnClickListener {
+                val isFull = viewModel?.getWaterState(8)
+                var tmp = if (isFull == true) {
+                    water9.setImageResource(R.drawable.ic_water_full_48dp)
+                    false
+                } else {
+                    water9.setImageResource(R.drawable.ic_water_empty_48dp)
+                    true
+                }
+                viewModel?.setWaterIsFull(8, tmp)
+            }
+
+            water10.setOnClickListener {
+                val isFull = viewModel?.getWaterState(9)
+                var tmp = if (isFull == true) {
+                    water10.setImageResource(R.drawable.ic_water_full_48dp)
+                    false
+                } else {
+                    water10.setImageResource(R.drawable.ic_water_empty_48dp)
+                    true
+                }
+                viewModel?.setWaterIsFull(9, tmp)
+            }
+
+            binding?.apply {
+                waterTrackerSaveButton.setOnClickListener { goBackToTrackerScreen() }
+            }
+        }
+    }
+
+    fun goBackToTrackerScreen() {
+        // TODO: save water intake to database
+        sharedViewModel.saveWaterIntake()
+        findNavController().navigate(R.id.action_tracker_water_to_navigation_tracker)
     }
 
     override fun onDestroyView() {

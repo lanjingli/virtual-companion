e6a46a73e251887ca8622dc4d14a0e55a99d8642
Leonard Li <jc9li@uwaterloo.ca>
2023-07-28 06:04:58 -0400

    basic UI layouts/stri

--- app/src/main/java/com/example/ece489acompanionapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/ece489acompanionapp/ui/home/HomeFragment.kt
@@ -1,13 +1,17 @@
 package com.example.ece489acompanionapp.ui.home
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.ece489acompanionapp.R
 import com.example.ece489acompanionapp.databinding.FragmentHomeBinding
+import java.time.LocalDateTime
 
 class HomeFragment : Fragment() {
 
@@ -17,6 +21,7 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -29,10 +34,27 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val currentHour = LocalDateTime.now().hour
+        var greetingsIndex : Int
+        if (currentHour in 5..11) {
+            greetingsIndex = 1
+        } else if (currentHour in 12..18) {
+            greetingsIndex = 2
+        } else if (currentHour in 19..21) {
+            greetingsIndex = 3
+        } else {
+            greetingsIndex = 4
         }
+        val greeting = resources.getStringArray(R.array.greetings)[greetingsIndex]
+        val currentUserName = getString(R.string.current_user_name)
+        textView.text = greeting + " " + currentUserName
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
     override fun onDestroyView() {
